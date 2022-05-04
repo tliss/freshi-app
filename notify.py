52c219f79eb645aac2dc27b4885ec86948e0cf69
Harshil Khara <kharaharshil@gmail.com>
2022-05-04 13:58:44 -0400

    Final push for notify.py to main

--- notify.py
+++ notify.py
@@ -3,21 +3,22 @@ from datetime import *
 from send_email import send_email
 
 # Creating a connection with DB
-db_string = "postgresql://postgres:Enter_your_postgres_password_here@localhost:5432/freshi"
+db_string = "postgresql://postgres:freshiapp123@localhost:5432/freshi"
 # Example db_string = "postgresql://postgres:mypassword123@localhost:5432/freshi"
 db = create_engine(db_string)
 
 
 #Function to check for notification need
 def notifier():
-    result_set = db.execute("SELECT name,creator_id,expiration_date,email FROM food,public.user where food.creator_id=public.user.id order by expiration_Date ASC")
+    result_set = db.execute("SELECT name,creator_id,expiration_date,days_before,email FROM food,public.user where food.creator_id=public.user.id order by expiration_Date ASC")
     today=datetime.today()
     print(today)  
     for result in result_set:  
         expiry_date=result[2]
-        email_info=str(result[3])
+        email_info=str(result[4])
         name=str(result[0])
-        deadline=expiry_date-timedelta(3) 
+        days_before=int(result[3])
+        deadline=expiry_date-timedelta(days_before) 
         print(deadline)
         if today >= deadline:
             print("Email will be sent")
